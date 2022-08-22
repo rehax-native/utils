ddd8a551a9729e7a3352fb84db2d3a93e51f14c2
Denis <denis@Macbook22.local>
2022-08-23 01:43:20 +0200

    Move constexpr methods to header

--- rehaxUtils/os/os.h
+++ rehaxUtils/os/os.h
@@ -2,6 +2,10 @@
 
 #include <iostream>
 
+#if __APPLE__
+#include <TargetConditionals.h>
+#endif
+
 namespace rehaxUtils {
 
 enum class OSName {
@@ -22,4 +26,63 @@ public:
   constexpr static bool isLinux();
 };
 
+}
+
+constexpr rehaxUtils::OSName rehaxUtils::OS::name() {
+  if (rehaxUtils::OS::isWindows()) {
+    return OSName::Windows;
+  }
+  if (rehaxUtils::OS::isMac()) {
+    return OSName::Mac;
+  }
+  if (rehaxUtils::OS::isIos()) {
+    return OSName::IOs;
+  }
+  // Android must come before linux, because it also is linux.
+  if (rehaxUtils::OS::isAndroid()) {
+    return OSName::Android;
+  }
+  if (rehaxUtils::OS::isLinux()) {
+    return OSName::Linux;
+  }
+}
+
+constexpr bool rehaxUtils::OS::isWindows() {
+  #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+  return true;
+  #else
+  return false;
+  #endif
+}
+
+constexpr bool rehaxUtils::OS::isMac() {
+  #if __APPLE__ && TARGET_OS_MAC
+  return true;
+  #else
+  return false;
+  #endif
+}
+
+constexpr bool rehaxUtils::OS::isIos() {
+  #if __APPLE__ && TARGET_OS_IPHONE
+  return true;
+  #else
+  return false;
+  #endif
+}
+
+constexpr bool rehaxUtils::OS::isAndroid() {
+  #if __ANDROID__
+  return true;
+  #else
+  return false;
+  #endif
+}
+
+constexpr bool rehaxUtils::OS::isLinux() {
+  #if __linux__
+  return true;
+  #else
+  return false;
+  #endif
 }
