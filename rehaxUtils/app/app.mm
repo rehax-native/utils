09760f2134997295807cbba08db0580a687daa0e
Denis <denisstadniczuk@gmail.com>
2022-12-08 14:37:28 +0100

    Fix observing theme

--- rehaxUtils/app/app.mm
+++ rehaxUtils/app/app.mm
@@ -1,6 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
 #include "app.h"
+#include <unordered_map>
 
 std::string rehaxUtils::App::getApplicationSupportDirectory() {
   NSArray * paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
@@ -22,21 +23,39 @@ std::string rehaxUtils::App::getApplicationGroupContainerDirectory(std::string a
   return [[url path] UTF8String];
 }
 
-static NSMutableDictionary * applicationThemeListeners = nil;
-static int nextListenerId = 0;
-
 @interface AppThemeListener : NSObject
 {
   @public
-  std::function<void(rehaxUtils::App::ApplicationTheme)> callback;
+  int nextListenerId;
+  bool didStartListening;
+  std::unordered_map<int, std::function<void(rehaxUtils::App::ApplicationTheme)>> applicationThemeListeners;
 }
+
++ (AppThemeListener *)sharedListener;
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context;
+
 @end
 
 @implementation AppThemeListener
 
++ (AppThemeListener *)sharedListener
+{
+  static AppThemeListener * shared = nil;
+  @synchronized(self) {
+    if (shared == nil) {
+      shared = [[self alloc] init];
+      shared->nextListenerId = 0;
+      shared->didStartListening = false;
+    }
+  }
+  return shared;
+}
+
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
-  callback(rehaxUtils::App::getApplicationTheme());
+  for (auto & pair : applicationThemeListeners) {
+    pair.second(rehaxUtils::App::getApplicationTheme());
+  }
 }
 
 @end
@@ -56,23 +75,21 @@ rehaxUtils::App::ApplicationTheme rehaxUtils::App::getApplicationTheme()
 
 rehaxUtils::App::ApplicationThemeListenerId rehaxUtils::App::addApplicationThemeChangeListener(std::function<void(rehaxUtils::App::ApplicationTheme)> callback)
 {
-  if (applicationThemeListeners == nil) {
-    applicationThemeListeners = [NSMutableDictionary new];
+  AppThemeListener.sharedListener->nextListenerId++;
+  
+  // Before the app has finished launching, adding observer will have no effect
+  // Therefore we add the observer only when we have an effectiveAppearance
+  if (!AppThemeListener.sharedListener->didStartListening && NSApp.effectiveAppearance.name != nil) {
+    [NSApp addObserver:AppThemeListener.sharedListener forKeyPath:@"effectiveAppearance" options:NSKeyValueObservingOptionNew context:nil];
+    AppThemeListener.sharedListener->didStartListening = true;
   }
-  nextListenerId++;
-  AppThemeListener * listener = [AppThemeListener new];
-  listener->callback = callback;
-  applicationThemeListeners[[NSNumber numberWithInt:nextListenerId]] = listener;
-  [NSApp addObserver:listener forKeyPath:@"effectiveAppearance" options:NSKeyValueObservingOptionNew context:nil];
-  return { nextListenerId };
+  AppThemeListener.sharedListener->applicationThemeListeners[AppThemeListener.sharedListener->nextListenerId] = callback;
+  return { AppThemeListener.sharedListener->nextListenerId };
 }
 
 void rehaxUtils::App::removeApplicationThemeChangeListener(rehaxUtils::App::ApplicationThemeListenerId listenerId)
 {
-  NSNumber * key = [NSNumber numberWithInt:listenerId.id];
-  if ([applicationThemeListeners objectForKey:key]) {
-    AppThemeListener * listener = applicationThemeListeners[key];
-    [NSApp removeObserver:listener forKeyPath:@"effectiveAppearance"];
-    [applicationThemeListeners removeObjectForKey:key];
+  if (AppThemeListener.sharedListener->applicationThemeListeners.contains(listenerId.id)) {
+    AppThemeListener.sharedListener->applicationThemeListeners.erase(listenerId.id);
   }
 }
